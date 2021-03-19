a60115cf75cf1bfa4b9fe8615dcc1e763247c8da
Reindert-Jan Ekker <info@codesensei.nl>
2021-03-19 01:57:52 +0100

    Starting Module 7: Migrations

--- carved_rock/store/models.py
+++ carved_rock/store/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from decimal import Decimal
+
+
+class ProductInStockQuerySet(models.QuerySet):
+    def in_stock(self):
+        return self.filter(stock_count__gt=0)
 
 
 class Product(models.Model):
@@ -7,6 +15,25 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(default="", blank=True)
     sku = models.CharField(verbose_name="Stock Keeping Unit", max_length=20, unique=True)
+    #slug = models.SlugField()
+
+    objects = models.Manager()
+    in_stock = ProductInStockQuerySet.as_manager()
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['price']
+        constraints = [
+             models.CheckConstraint(check=models.Q(price__gte=0),
+                                    name="price_not_negative")
+        ]
+
+    def get_absolute_url(self):
+        return reverse("store:product-detail", kwargs={'pk': self.id})
 
     def __str__(self):
         return self.name
@@ -23,6 +50,10 @@ class ProductImage(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     products = models.ManyToManyField('Product', related_name="categories")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ['name']
 
     def __str__(self):
         return self.name
